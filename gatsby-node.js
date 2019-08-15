77dadf3d2342390481a554fa882dfae216b024f7
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-08-15 17:51:50 +0800

    working my way through gnode.js file

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,32 @@
-exports.onCreateNode = ({ node }) => {
-    if (node.internal.type === `MarkdownRemark`) {
-      console.log(node.internal.type)
-    }
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
   }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(JSON.stringify(result, null, 4))
+}
